2033defc41021555fef2cd94bf6417e6031e41e0
yarayk <yaroslav.bogdanov.2005@mail.ru>
2026-02-17 15:13:49 +0500

    Done

--- sql_queries.py
+++ sql_queries.py
@@ -1,32 +1,34 @@
-# Вывести топ 5 самых коротких по длительности перелетов.
-# Duration - разница между scheduled_arrival и scheduled_departure.
-# В ответе должно быть 2 колонки [flight_no, duration]
+# Топ 5 самых коротких перелетов по длительности
+# Колонки: [flight_no, duration]
 TASK_1_QUERY = """
+SELECT
+    flight_no,
+    scheduled_arrival - scheduled_departure AS duration
+FROM flights
+ORDER BY duration ASC
+LIMIT 5;
 """
-#  flight_no | duration
-# -----------+----------
-#  PG0235    | 00:25:00
-#  PG0234    | 00:25:00
-#  PG0233    | 00:25:00
-#  PG0235    | 00:25:00
-#  PG0234    | 00:25:00
 
-
-# Вывести топ 3 рейса по числу упоминаний в таблице flights
-# количество упоминаний которых меньше 50
-# В ответе должно быть 2 колонки [flight_no, count]
+# Топ 3 рейса по числу упоминаний (меньше 50)
+# Колонки: [flight_no, count]
 TASK_2_QUERY = """
+SELECT
+    flight_no,
+    COUNT(*) AS count
+FROM flights
+GROUP BY flight_no
+HAVING COUNT(*) < 50
+ORDER BY count DESC
+LIMIT 3;
 """
-#  flight_no | count
-# -----------+-------
-#  PG0260    |    27
-#  PG0371    |    27
-#  PG0310    |    27
 
-# Вывести число перелетов внутри одной таймзоны
-# Нужно вывести 1 значение в колонке count
+# Число перелетов внутри одной таймзоны
+# Колонка: [count]
 TASK_3_QUERY = """
+SELECT
+    COUNT(*) AS count
+FROM flights f
+JOIN airports_data a_dep ON f.departure_airport = a_dep.airport_code
+JOIN airports_data a_arr ON f.arrival_airport = a_arr.airport_code
+WHERE a_dep.timezone = a_arr.timezone;
 """
-#  count
-# --------
-#  16824
